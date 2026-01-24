13915f25c0b37c04a6180b97bbc2bcaf0bce1a9e
budos <budos2@uniba.sk>
2026-01-24 11:48:20 +0100

    Connection, sending info, events

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,96 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as net from "net";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "xrdebugext" is now active!');
+const AST_PROXY_HOST = "192.168.0.103";
+const AST_PROXY_PORT = 4720;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('xrdebugext.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from XRDebugExt!');
-	});
+let client: net.Socket | null = null;
+let connected = false;
+const messageQueue: string[] = [JSON.stringify({ type: "id", content: "Extension" })];
+let currentPath: string = "";
 
-	context.subscriptions.push(disposable);
+// Odoslanie správy alebo bufferovanie
+function sendPath(path: string) {
+    const message = JSON.stringify({ type: "path", content: path }) + "\n";
+    currentPath = path;
+    sendMessage(message);
+    
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+
+function sendMessage(msg: string){
+    if (connected && client) {
+        client.write(msg);
+    } else {
+        messageQueue.push(msg);
+    }
+
+}
+
+// Inicializácia TCP spojenia s reconnect logikou
+function initTcpConnection() {
+    client = new net.Socket();
+
+    client.connect(AST_PROXY_PORT, AST_PROXY_HOST, () => {
+        console.log(`Connected to AST proxy at ${AST_PROXY_HOST}:${AST_PROXY_PORT}`);
+        connected = true;
+
+        // Odoslať všetky správy z queue
+        while (messageQueue.length > 0) {
+            const msg = messageQueue.shift();
+            if (msg && client) {
+                client.write(msg);
+            }
+        }
+    });
+
+    client.on("data", (data) => {
+        console.log("Received from SERVER:", data.toString());
+    });
+
+    client.on("close", () => {
+        console.log("SERVER connection closed, reconnecting in 2s...");
+        connected = false;
+        setTimeout(initTcpConnection, 2000);
+    });
+
+    client.on("error", (err) => {
+        console.error("SERVER connection error:", err);
+    });
+}
+
+// Aktivácia extension
+export function activate(context: vscode.ExtensionContext) {
+    console.log("XRDebugExt activated");
+
+    initTcpConnection();
+
+    // Posielanie cesty pri prepnutí editoru
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+        if (editor?.document) sendPath(editor.document.fileName);
+    }, null, context.subscriptions);
+
+    vscode.debug.onDidStartDebugSession((session) => {
+    console.log("Debug session started:", session.name);
+
+    // napr. pošli aktuálny otvorený súbor
+    const editor = vscode.window.activeTextEditor;
+    if (editor?.document) {
+        sendPath(editor.document.fileName);
+    }
+    
+
+}, null, context.subscriptions);
+
+}
+
+// Deaktivácia extension
+export function deactivate() {
+    if (client) {
+        client.end();
+        client.destroy();
+        client = null;
+    }
+    connected = false;
+}
